d737bef284fb5383dcb78c79d56b8ee8b91f456a
Polly <apollinaria9203@yandex.by>
2021-08-11 09:01:16 +0300

    Maybe it work

---
+++ ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp1
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=consoleappdb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Book book1 = new Book { Name = "Vanish", Author = "Sophie Jordan", Annotation = "An Impossible Romance.Bitter Rivalries.Deadly Choices.To save..." };
+                Book book2 = new Book { Name = " Sharp Objects", Author = "Gillian Flynn", Annotation = "Fresh from a brief stay at a psych hospital, reporter Camille..." };
+                Book book3 = new Book { Name = "Peter Pan", Author = "J. M. Barrie", Annotation = "Peter Pan is a character created by Scottish novelist and playwright..." };
+
+                db.Books.Add(book1);
+                db.Books.Add(book2);
+                db.Books.Add(book3);
+                db.SaveChanges();
+
+                var books = db.Books.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (Book b in books)
+                {
+                    Console.WriteLine($"Book {b.Id} - {b.Name} - {b.Author} - {b.Annotation}");
+                }
+
+                List<Book> booksList = new List<Book>(5);
+                booksList.Add(new Book() { Name = "name1", Author = "author1", Annotation = "annotation1" });
+                booksList.Add(new Book() { Name = "name2", Author = "author2", Annotation = "annotation2" });
+                booksList.Add(new Book() { Name = "name3", Author = "author3", Annotation = "annotation3" });
+                booksList.Add(new Book() { Name = "name4", Author = "author4", Annotation = "annotation4" });
+                booksList.Add(new Book() { Name = "name5", Author = "author5", Annotation = "annotation5" });
+
+                foreach (Book b in booksList)
+                {
+                    db.Books.Add(b);
+                    db.SaveChanges();
+
+                }
+
+                var books2 = db.Books.ToList();
+                Console.WriteLine("Список объектов №2:");
+                foreach (Book b in books2)
+                {
+                    Console.WriteLine($"Book {b.Id} - {b.Name} - {b.Author} - {b.Annotation}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
